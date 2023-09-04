2b920894e002b65822f33290b86cc62a0ef73a90
Arthur <eng.arthurpaschoalotto@gmail.com>
2023-09-04 16:55:28 -0300

    ex1
    Signed-off-by: Arthur <eng.arthurpaschoalotto@gmail.com>

---
+++ DevC++/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Numero inteiro: %d", 12);
+    printf("\nNumero real: %f", 3.141592);
+    printf("\nNumero real com 2 casas: %.2f", 3.141592);
+    printf("\nNumero: %d", 12);
+    printf("\nTexto: %c", 'a');
+    printf("\nTexto: %s", "testando");
+    return 0;
+}
+
+
